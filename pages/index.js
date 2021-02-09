8b1287f3510c7a432a00c67d8eb4b95647df9056
Gabriel <gabrielm.linassi@gmail.com>
2021-02-08 21:27:17 -0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Bem vindo a programação, Lukas!</h1>
+      <span>
+        Me adicione no{" "}
+        <a
+          className={styles.link}
+          href="https://www.facebook.com/lucas.parnowlamb.7"
+        >
+          Facebook
+        </a>
+      </span>
     </div>
   );
 }
